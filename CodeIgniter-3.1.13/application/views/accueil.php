500db012822e58dae6842dd5048ca8ed929703f5
stiti <moncef.stiti@icloud.com>
2024-05-18 14:52:46 +0200

    Test d'une page d'accueil

---
+++ CodeIgniter-3.1.13/application/views/accueil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<?php include '/layout/header_not_logged_dark.php';?>
+<head>
+    <title>Page d'accueil</title>
+</head>
+<body>
+    <h1>Bienvenue sur notre site !</h1>
+    <p>Ceci est la page d'accueil de notre application.</p>
+</body>
+</html>
