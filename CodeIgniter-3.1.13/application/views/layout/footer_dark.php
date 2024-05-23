e55821b0fef1cb0a09a64a48c516edb959b7cf46
stiti <moncef.stiti@gmail.com>
2024-05-23 10:59:40 +0200

    Correction de bugs

--- CodeIgniter-3.1.13/application/views/layout/footer_dark.php
+++ CodeIgniter-3.1.13/application/views/layout/footer_dark.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/footer_dark'); ?>">>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/footer_dark'); ?>">
 </head>
 <body>
     <footer class="footer">
