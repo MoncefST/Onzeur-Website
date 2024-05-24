664c17410e600a976c753a3a1afbda39b8365c4c
orfao <marcoorfao2003@gmail.com>
2024-05-24 10:37:57 +0200

    favicon mis en place

--- CodeIgniter-3.1.13/application/views/layout/header_dark.php
+++ CodeIgniter-3.1.13/application/views/layout/header_dark.php
@@ -5,5 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?=link_tag('assets/css/header_not_logged_dark')?>
     <?=link_tag('assets/img/Logo_ONZEUR.png')?>
+    <?=link_tag(array(
+        'href'=>'assets/img/Logo_ONZEUR.png',
+        'rel'=>'icon',
+        'type'=>'image/x-icon'
+    ))?>
     <?=link_tag('assets/css/footer_dark.css')?>
 </head>
