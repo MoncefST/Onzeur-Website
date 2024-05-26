f2644d2e5ce025187e5b64db62d8fd06df0c4f4c
stiti <moncef.stiti@gmail.com>
2024-05-26 15:58:11 +0200

    Encore des optimisations

--- CodeIgniter-3.1.13/application/views/dashboard.php
+++ CodeIgniter-3.1.13/application/views/dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Dashboard - Onzeur</title>
     <link rel="stylesheet" href="<?php echo base_url('assets/css/dashboard'); ?>">
 </head>
 <body>
